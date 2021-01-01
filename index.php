90877ca51e0ff44f3df8e584e325144a5ccaff01
Alexandre S. Silva <alexandresoussilva@gmail.com>
2021-01-01 13:30:53 -0300

    mais uma atualização

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
     echo"Olá mundo!";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula github</title>
+</head>
+<body>
+    <p>Olá mundo!</p>
+</body>
+</html>
